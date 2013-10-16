dcfdb3b6cad89cf6c17a6c533a7fd0a2b4dbf00d
junereycasuga <junerey.casuga@gmail.com>
2013-10-16 23:00:53 +0800

    added octopost.rb

---
+++ octopost.rb
@@ -0,0 +1,12 @@
+# Title: Octopost
+# Author: Junerey Casuga (@junereytionX)
+# Description:
+# 	Octopost is graphical user interface for generating blog posts and pages.
+# 	It has an editor for markdown showing the HTML result real-time.
+# 	It also allows you to deploy your post and pages.
+
+require 'launchy'
+
+module Octopost
+	
+end
